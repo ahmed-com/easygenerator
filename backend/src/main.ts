024566af162cf7764d2afde45c811af228727e4b
ahmed-com <ahmed0grwan@gmail.com>
2025-03-13 03:35:17 +0200

    layed down the general structure and packages to use

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Easy Generator API')
+    .setDescription('The Easy Generator API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config, {
+    operationIdFactory: (_: string, methodKey: string) => methodKey,
+  });
+  SwaggerModule.setup('api-docs', app, documentFactory, {
+    jsonDocumentUrl: '/api-docs-json',
+    yamlDocumentUrl: '/api-docs-yaml',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
